7b1457f91eb4a9f9270bccc469ccff4284d72351
第一个项目 <876516151@qq.com>
2016-01-19 10:39:30 +0800

    Initial Commit

---
+++ CookMan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CookMan
+//
+//  Created by 尹晓明 on 16/1/19.
+//  Copyright © 2016年 尹晓明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CookManTests/CookManTests.m
@@ -0,0 +1,39 @@
+//
+//  CookManTests.m
+//  CookManTests
+//
+//  Created by 尹晓明 on 16/1/19.
+//  Copyright © 2016年 尹晓明. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CookManTests : XCTestCase
+
+@end
+
+@implementation CookManTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
