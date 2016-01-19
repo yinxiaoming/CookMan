c738ceade2985b28945f6bd6778c140218be9b9a
wu2723056 <80972297@qq.com>
2016-01-19 19:50:46 +0800

    left
    leftDraw

---
+++ CookMan/LeftViewController.h
@@ -0,0 +1,13 @@
+//
+//  LeftViewController.h
+//  CookMan
+//
+//  Created by wy on 16/1/19.
+//  Copyright © 2016年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftViewController : UIViewController
+
+@end
