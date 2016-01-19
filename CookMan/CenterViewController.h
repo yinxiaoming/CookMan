8d9acf5a5578bade681ce859ac941fd52a45b823
wu2723056 <80972297@qq.com>
2016-01-19 20:21:29 +0800

    LeftVC
    LeftVC

---
+++ CookMan/CenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.h
+//  UI自定义侧滑控制器
+//
+//  Created by wy on 15/11/10.
+//  Copyright © 2015年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterViewController : UIViewController
+
+@end
